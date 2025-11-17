31d4448c9336974706bff31dfdfed37863306e17
pasalkardivya <paslkardivya@gmail.com>
2025-11-17 19:00:53 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/example/strisakti/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/strisakti/ui/screen/HomeScreen.kt
@@ -1,14 +1,25 @@
 package com.example.strisakti.ui.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
@@ -16,17 +27,36 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         "New Updates",
         "Start for Women",
         "Law for Women",
-        "Advantages for Women"
+        "Advantages for Women",
+        " periods date treak",
+        "chat with online"
     )
     Column(
         modifier
     ) {
         LazyVerticalGrid(columns = GridCells.Adaptive(200.dp)) {
-            items(options) {option ->
-                Button({}) {
-                    Text(option)
+            items(options) { option ->
+                Card(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(180.dp)              // Square 180dp
+                        .clickable { /* TODO: handle click */ },
+                    shape = RoundedCornerShape(12.dp),
+                    elevation = CardDefaults.cardElevation(8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = option,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
         }
+
     }
 }
